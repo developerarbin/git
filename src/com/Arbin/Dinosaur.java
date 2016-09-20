320deafda6adf55a69494216c87c814f698edf0c
Arbin Rai <hello>
2016-09-20 11:30:14 -0500

    Created Dinosaur class and tested roar message

---
+++ src/com/Arbin/Dinosaur.java
@@ -0,0 +1,10 @@
+package com.Arbin;
+
+/**
+ * Created by Arbin Rai on 9/20/2016.
+ */
+public class Dinosaur {
+    public void roar(){
+        System.out.println("GRRR!!!!");
+    }
+}
---
+++ src/com/Arbin/DinosaurSupervisor.java
@@ -0,0 +1,17 @@
+package com.Arbin;
+
+/**
+ * Created by Arbin Rai on 9/20/2016.
+ */
+public class DinosaurSupervisor {
+    public static void main (String[] args){
+        Dinosaur t_rex = new Dinosaur();
+        Dinosaur velociraptor=new Dinosaur();
+        Dinosaur diplodous=new Dinosaur();
+
+        t_rex.roar();
+        velociraptor.roar();
+        diplodous.roar();
+
+    }
+}
